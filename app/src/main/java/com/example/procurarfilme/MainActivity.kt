91c7def3c3d98de28f1f24791290b7735f3d83aa
Pedro Gomes <aformigamorreupedro@gmail.com>
2024-09-30 15:23:25 +0100

    inicio layout

---
+++ app/src/main/java/com/example/procurarfilme/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.procurarfilme
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        listarFilmes()
+    }
+    fun listarFilmes(){
+        val llContainer = findViewById<LinearLayout>(R.id.listagem)
+        for(i in 6 downTo 0 step 1){
+            val newTextView = LayoutInflater.from(this).inflate(R.layout.filme, null, false)
+            llContainer.addView(newTextView)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/procurarfilme/classes/filme.kt
@@ -0,0 +1,6 @@
+package com.example.procurarfilme.classes
+
+import java.io.Serializable
+
+class filme (var titulo:String, var anoLancamento:Int):Serializable{
+}
